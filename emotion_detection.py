6b3d6a1ce51e9e977d50239f706a6ed4efc177cb
Haider Ali <haider.ali07304@gmail.com>
2024-07-20 19:10:01 +0500

    Emotion

---
+++ emotion_detection.py
@@ -0,0 +1,12 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=data, headers=headers)
+    return response.json()['text']  
+
+if __name__ == "__main__":
+
+    print(emotion_detector("I love this new technology"))
